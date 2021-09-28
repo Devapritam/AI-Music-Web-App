e5c66ade74a8c7aebb6f5e1c71b74dad2be64dc4
Devapritam Sharma <dsarma1980@gmail.com>
2021-09-28 14:09:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+song1 = "";
+song2 = "";
+
+function preload() {
+    song1 = loadSound("harry potter.mp3");
+    song2 = loadSound("Unstoppable.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 500, 400);
+}
